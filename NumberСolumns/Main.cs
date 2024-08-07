325c2fece7aeecbda46f0b25dce43d71bad89681
Sergei Prutsakov <psn206@mail.ru>
2024-08-07 08:23:15 +0300

    ок

---
+++ NumberСolumns/Main.cs
@@ -0,0 +1,31 @@
+﻿using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumberСolumns
+{
+    [Transaction(TransactionMode.Manual)]
+    public class Main : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            UIApplication uiApp = commandData.Application;
+            UIDocument uiDoc = uiApp.ActiveUIDocument;
+            Document doc = uiDoc.Document;
+
+            List<Element> сolumns = new FilteredElementCollector(doc)
+                .OfCategory(BuiltInCategory.OST_StructuralColumns)
+                .WhereElementIsNotElementType()
+                .Cast<Element>()
+                .ToList();
+
+            TaskDialog.Show("?", $"Количество колонн= {сolumns.Count.ToString()}");
+            return Result.Succeeded;
+        }
+    }
+}
